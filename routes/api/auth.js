2d57eff8c3c7551b5e267a691b6d1f830640b342
Robert <4b7e3150-930f-41a1-8205-3a84bfd26d22@anonaddy.me>
2021-04-11 17:28:12 -0600

    Established Auth route
    Can now authenticate a user. Verified the middleware works via postman.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,11 +4,23 @@ const router = express.Router();
 // Utilize the middelware in middleware/auth.js
 const auth = require('../../middleware/auth.js');
 
+// Bring in the user model
+const User = require('../../models/User');
+
 //@Route    GET api/auth
 //@Desc     test route
 //@Access   Public route
 
 // By using the "auth" variable here, this is now a protected route
-router.get('/', auth, (req, res) => res.send('Auth Route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    // We can access req.user because this is a protected route and every protected route has access to the decoded.user from the middleware.
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error, Routes/api/auth.js router.get');
+  }
+});
 
 module.exports = router;
