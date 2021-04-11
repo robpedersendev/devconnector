439230fe0e1442dff799e47f5a2732108cc4ab70
Robert <4b7e3150-930f-41a1-8205-3a84bfd26d22@anonaddy.me>
2021-04-11 17:14:17 -0600

    Added the middleware to routes/api/auth.js
    Verified the middleware works via postman.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+// Utilize the middelware in middleware/auth.js
+const auth = require('../../middleware/auth.js');
+
 //@Route    GET api/auth
 //@Desc     test route
 //@Access   Public route
-router.get('/', (req,res) => res.send('Auth Route'))
+
+// By using the "auth" variable here, this is now a protected route
+router.get('/', auth, (req, res) => res.send('Auth Route'));
 
 module.exports = router;
