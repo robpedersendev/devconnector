8c97f75cb70e8dcfdbff8d8adaeaa61209ea5c9c
Bob <4b7e3150-930f-41a1-8205-3a84bfd26d22@anonaddy.me>
2021-06-28 22:39:47 -0600

    Built out the route for retrieving the current users profile, and am currently building the create/update functionality for the users profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,91 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
-//@Route    GET api/profile
-//@Desc     test route
-//@Access   Public route
-router.get('/', (req, res) => res.send('Profile Route'));
+// Import the models for the DB
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+//@Route    GET api/profile/me
+//@Desc     Current users profile
+//@Access   Private route
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//@Route    Post api/profile
+//@Desc     Create or update user profile
+//@Access   Private route
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required").not().isEmpty(),
+      check("skills", "Skills is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    // If there are errors
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+
+    // Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.company = website;
+    if (location) profileFields.company = location;
+    if (bio) profileFields.company = bio;
+    if (status) profileFields.company = status;
+    if (githubusername) profileFields.company = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(",").map((skill) => skill.trim());
+    }
+    // Build the social object.
+    // We have to initialize the social object first, otherwise it will return undefined
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = location.facebook;
+    if (linkedin) profileFields.social.linkedin = bio.linkedin;
+    if (instagram) profileFields.social.instagram = bio.instagram;
+
+    console.log(profileFields.skills);
+    res.send("testing");
+  }
+);
 
 module.exports = router;
